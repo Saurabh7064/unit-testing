cde403f825e822b4b97800e01c4fec06b57fb5b0
Saurabh <saurabh.kumar@epsilon.com>
2020-09-17 15:11:13 +0530

    verify in mockito

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # unit-testing
+**Verify**-it verifies if specifics method is called or not with correct input.
+Very useful when a method doesn't return anything and mostly used there.
---
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
@@ -0,0 +1,71 @@
+package com.in28minutes.unittesting.unittesting.business;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.in28minutes.unittesting.unittesting.data.SomeDataService;
+import org.junit.Ignore;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ListMockTest {
+
+    List<String> mock = mock(List.class);
+
+    @Test
+    public void size_basic() {
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
+    }
+
+    @Test
+    public void returnDifferentValues() {
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters() {
+        when(mock.get(0)).thenReturn("in28Minutes");
+        assertEquals("in28Minutes", mock.get(0));
+        assertEquals(null, mock.get(1));
+    }
+
+    @Test
+    public void verificationBasics() {
+        // SUT
+        String value1 = mock.get(0);
+        String value2 = mock.get(1);
+
+        // Verify
+        verify(mock).get(0);
+        //verify(mock).get(4); fails because we are not passing 4 in mock.get(0)
+
+        verify(mock, times(2)).get(anyInt());
+        verify(mock, atLeast(1)).get(anyInt());
+        verify(mock, atLeastOnce()).get(anyInt());
+        verify(mock, atMost(2)).get(anyInt());
+        verify(mock, never()).get(2);
+    }
+
+
+}
